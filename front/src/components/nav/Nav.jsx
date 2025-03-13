9b08de8d8667f367c5ca38f2f1da0596bd5c9ac3
Gustav-Br <99103909+Gustav-Br@users.noreply.github.com>
2025-03-13 15:34:44 -0300

    Modificando estilos en Nav

--- front/src/components/nav/Nav.jsx
+++ front/src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import style from "./Nav.module.css";
 import SearchBar from "../searchbar/SearchBar";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Nav = ({ onSearch }) => {
@@ -8,9 +8,9 @@ const Nav = ({ onSearch }) => {
     return (
         <div className={style.nav}>
             <div className={style.navLinks}>
-                <Link to='/about'>About</Link>
-                <Link to='/home'>Home</Link>
-                <Link to='/favorites'>Favorites</Link>
+                <NavLink to='/about' className={({ isActive }) => (isActive ? style.active : "")}>About</NavLink>
+                <NavLink to='/home' className={({ isActive }) => (isActive ? style.active : "")}>Home</NavLink>
+                <NavLink to='/favorites' className={({ isActive }) => (isActive ? style.active : "")}>Favorites</NavLink>
             </div>
             <SearchBar onSearch={onSearch} />
         </div>
